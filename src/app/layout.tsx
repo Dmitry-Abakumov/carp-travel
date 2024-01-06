151f59595d195fa3a9ab4fa149219bc9cf9d16d4
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-01-06 21:01:24 +0200

    Adds favicon and ogp images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 import Header from "@/components/Header";
 
@@ -20,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="/images/ogp/ogp.jpg" />
+      </Head>
       <body className={`${inter.className} text-primary-text-color`}>
         <Header />
         {children}
